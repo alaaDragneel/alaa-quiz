bf34f0fff88bfe726eb849a2388dc4c462e20466
alaaDragneel <moaalaa16@gmail.com>
2018-09-06 15:17:38 +0200

    INsstalling Chatbot

---
+++ routes/botman.php
@@ -0,0 +1,14 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+
+$botman->hears('It just works', function ($bot) {
+    $bot->reply('Yep 🤘');
+}); 
+
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
